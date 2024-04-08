b587a108e0441561f06f200df6d1e1d32be2353a
Lê Ý <duongy96@gmail.com>
2024-04-09 02:20:51 +0700

    connect api

--- src/components/layouts/footer/BaseFooter.vue
+++ src/components/layouts/footer/BaseFooter.vue
@@ -1,56 +1,6 @@
 <template>
   <footer class="bg-white" aria-labelledby="footer-heading">
-    <h2 id="footer-heading" class="sr-only">Footer</h2>
     <div class="mx-auto max-w-7xl px-6 pb-8 pt-16 sm:pt-24 lg:px-8 lg:pt-32">
-      <div class="xl:grid xl:grid-cols-3 xl:gap-8">
-        <img
-          class="h-7"
-          src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-          alt="Company name"
-        />
-        <div class="mt-16 grid grid-cols-2 gap-8 xl:col-span-2 xl:mt-0">
-          <div class="md:grid md:grid-cols-2 md:gap-8">
-            <div>
-              <h3 class="text-sm font-semibold leading-6 text-gray-800">Photography</h3>
-              <ul role="list" class="mt-6 space-y-4">
-                <li v-for="item in navigation.photography" :key="item.name">
-                  <a
-                    :href="item.href"
-                    class="text-sm leading-6 text-gray-600 hover:text-gray-800"
-                    >{{ item.name }}</a
-                  >
-                </li>
-              </ul>
-            </div>
-            <div class="mt-10 md:mt-0">
-              <h3 class="text-sm font-semibold leading-6 text-gray-800">Content</h3>
-              <ul role="list" class="mt-6 space-y-4">
-                <li v-for="item in navigation.content" :key="item.name">
-                  <a
-                    :href="item.href"
-                    class="text-sm leading-6 text-gray-600 hover:text-gray-800"
-                    >{{ item.name }}</a
-                  >
-                </li>
-              </ul>
-            </div>
-          </div>
-          <div class="md:grid md:grid-cols-2 md:gap-8">
-            <div>
-              <h3 class="text-sm font-semibold leading-6 text-gray-800">Company</h3>
-              <ul role="list" class="mt-6 space-y-4">
-                <li v-for="item in navigation.company" :key="item.name">
-                  <a
-                    :href="item.href"
-                    class="text-sm leading-6 text-gray-600 hover:text-gray-800"
-                    >{{ item.name }}</a
-                  >
-                </li>
-              </ul>
-            </div>
-          </div>
-        </div>
-      </div>
       <div
         class="mt-16 border-t border-gray-900/10 pt-8 sm:mt-20 lg:mt-24 lg:flex lg:items-center lg:justify-between"
       >
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -5,35 +5,46 @@
       <h1 class="text-4xl font-bold tracking-tight sm:text-6xl mt-24 mb-16 text-neutral-800">
         Alana
       </h1>
-      <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-4">
+      <div class="grid sm:grid-cols-2 lg:grid-cols-4 gap-x-4 gap-y-16">
         <div
-          :class="cn('flex gap-2 flex-col', { 'lg:col-span-2 self-start': product.span === 2 })"
-          v-for="product in products1"
-          :key="product.id"
+          :class="
+            cn('flex gap-2 flex-col', {
+              'lg:col-span-2 self-start':
+                album.cover.width / album.cover.height > 1.3 || album.cover.mime.includes('video/')
+            })
+          "
+          v-for="album in albums"
+          :key="album.id"
         >
           <img
             :class="
-              cn('aspect-[3/4] flex-grow object-cover', { 'aspect-[3/2]': product.span === 2 })
+              cn('aspect-[3/4] flex-grow object-cover', {
+                'aspect-[3/2]':
+                  album.cover.width / album.cover.height > 1.3 ||
+                  album.cover.mime.includes('video/')
+              })
             "
-            :src="product.imageSrc"
-            :alt="product.imageAlt"
-            v-if="!!product.imageSrc"
+            :src="'http://localhost:1337' + album.cover.url"
+            :alt="album.cover.alternativeText"
+            v-if="!album.cover.mime.includes('video/')"
           />
           <video
             controls
-            v-if="!!product.videoSrc"
+            v-if="album.cover.mime.includes('video/')"
             :class="
               cn('aspect-[3/4] bg-red-300 flex-grow object-cover', {
-                'aspect-[3/2]': product.span === 2
+                'aspect-[3/2]':
+                  album.cover.width / album.cover.height > 1.3 ||
+                  album.cover.mime.includes('video/')
               })
             "
           >
-            <source :src="product.videoSrc" type="video/mp4" />
+            <source :src="'http://localhost:1337' + album.cover.url" type="video/mp4" />
             Your browser does not support the video tag.
           </video>
           <div>
-            <p class="font-bold text-neutral-800 line-clamp-1">{{ product.name }}</p>
-            <p class="font-light text-neutral-600 line-clamp-1">{{ product.description }}</p>
+            <p class="font-bold text-neutral-800 line-clamp-1">{{ album.name }}</p>
+            <p class="font-light text-neutral-600 line-clamp-1">{{ album.description }}</p>
           </div>
         </div>
       </div>
@@ -47,6 +58,68 @@ import BaseSection from '@/components/layouts/BaseSection.vue'
 import BreadScrumb from '@/components/layouts/BreadScrumb.vue'
 import { getRoute } from '@/router'
 import { cn } from '@/utils/cn'
+import { onMounted, ref, watchEffect } from 'vue'
+
+type Category = {
+  id: number
+  name: string
+  description: string
+  type: string
+}
+
+type Thumbnail = {
+  name: string
+  hash: string
+  ext: string
+  mime: string
+  path: null
+  width: number
+  height: number
+  size: number
+  sizeInBytes: number
+  url: string
+}
+type Media = {
+  id: number
+  name: string
+  alternativeText?: string
+  caption?: string
+  width: number
+  height: number
+  formats?: {
+    thumbnail: Thumbnail
+  }
+  hash: string
+  ext: string
+  mime: string
+  size: number
+  url: string
+  previewUrl?: string
+  provider: string
+  provider_metadata?: string
+  createdAt: string
+  updatedAt: string
+}
+
+type Album = {
+  id: number
+  name: string
+  description: string
+  category: Category
+  cover: Media
+  medias: Media[]
+}
+const albums = ref<Album[]>([])
+
+watchEffect(() => {
+  console.log('albums', albums.value)
+})
+
+onMounted(() => {
+  fetch('http://localhost:1337/api/albums?populate=deep').then(async (rs) => {
+    albums.value = (await rs.json()) as Album[]
+  })
+})
 
 const pages = [{ name: 'Home', to: getRoute('home').path, current: true }]
 const products1 = [
